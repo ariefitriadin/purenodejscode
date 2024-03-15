5361278b241ad842c3fc4fe5cd76175ad12cb7f6
Arie Fitriadin <arie.fitriadin@gmail.com>
2024-03-15 19:14:57 +0700

    add app.js

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+
 const app = express();
-const port =  process.env.PORT || 3000;
+
+// Initialize MongoDB Memory Server
+async function connectToMongoDB() {
+    const mongod = await MongoMemoryServer.create();
+    const mongoUri = mongod.getUri();
+
+    await mongoose.connect(mongoUri);
+}
+
+app.use(express.json());
+
+// Connect to the MongoDB in-memory server
+connectToMongoDB().then(() => {
+    console.log('Connected to MongoDB in-memory server');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB in-memory server:', error);
+    process.exit(1);
+});
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // routes
-app.use('/', require('./routes/profile')());
+app.use('/comments', require('./routes/comments'));
+app.use('/users', require('./routes/users'));
+app.use('/', require('./routes/profile'));
+
+module.exports = app;
 
 // start server
-const server = app.listen(port);
-console.log('Express started. Listening on %s', port);
+if (require.main === module) {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log('Express started. Listening on port %s', port);
+    });
+};
+
